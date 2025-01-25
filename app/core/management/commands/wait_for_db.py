b0878aa398d345fbfaa579e6be9a8f9450c9726d
Bekarys <bek.arys@inbox.ru>
2025-01-24 18:44:55 -0800

    change in lint

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 import time
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
-from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
